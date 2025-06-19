3dd00b7fc1324a8a009ef00ab9cd673bb2781704
Marsel Faizullin <f1marsel@gmail.com>
2025-06-19 12:53:38 +0300

    Создана Третья ветка и внесены изменения в классы

--- src/main/java/org/example/Box.java
+++ src/main/java/org/example/Box.java
@@ -3,6 +3,13 @@ package org.example;
 public class Box {
     private String size;
 
+    @Override
+    public String toString() {
+        return "Box{" +
+                "size='" + size + '\'' +
+                '}';
+    }
+
     public String getSize() {
         return size;
     }
